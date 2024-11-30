67170971c347e12a02981111c4c3d7d19996c945
Ajayi Marvellous <66161068+Marvellous890@users.noreply.github.com>
2024-11-30 16:22:01 +0000

    updated splitfile to use read stream

--- splitfile.js
+++ splitfile.js
@@ -1,4 +1,4 @@
-import { statSync, openSync, readSync, writeFileSync, closeSync, createWriteStream, readFileSync, unlinkSync, readdirSync } from 'fs';
+import { statSync, openSync, readSync, writeFileSync, closeSync, createReadStream, createWriteStream, readFileSync, unlinkSync, readdirSync } from 'fs';
 import { join, } from 'path';
 
 import { fileURLToPath } from 'url';
@@ -9,23 +9,28 @@ const __dirname = dirname(__filename);
 
 // Function to split the file into parts based on size
 function splitFileBySize(filePath, partSize) {
-    const fileSize = statSync(filePath).size;
     let bytesRead = 0;
     let partNumber = 1;
-    const fileDescriptor = openSync(filePath, 'r');
 
-    while (bytesRead < fileSize) {
-        const bufferSize = Math.min(partSize, fileSize - bytesRead);
-        const buffer = Buffer.alloc(bufferSize);
-        const bytesReadNow = readSync(fileDescriptor, buffer, 0, bufferSize, bytesRead);
+    const readStream = createReadStream(filePath, { highWaterMark: partSize });
+
+    readStream.on('data', (chunk) => {
         const partFileName = `${filePath}.part${partNumber}`;
-        writeFileSync(partFileName, buffer.slice(0, bytesReadNow));
+        const writeStream = createWriteStream(partFileName);
+        writeStream.write(chunk);
+        writeStream.end();
         console.log(`Created part ${partNumber}: ${partFileName}`);
-        bytesRead += bytesReadNow;
+        bytesRead += chunk.length;
         partNumber++;
-    }
+    });
 
-    closeSync(fileDescriptor);
+    readStream.on('end', () => {
+        console.log('File split completed.');
+    });
+
+    readStream.on('error', (err) => {
+        console.error('Error reading file:', err);
+    });
 }
 
 // Function to join the parts back into a single file synchronously without sorting
@@ -45,7 +50,7 @@ function joinFilesSync(partFiles, outputFileName) {
 }
 
 // Example usage
-const filePath = __dirname + '/dlfiles/Milan Jovanovic - Modular Monolith Architecture.zip.002'; // Change this to the path of your file
+const filePath = __dirname + '/fg-06.bin'; // Change this to the path of your file
 const partSize = 2_000_000_000; // Specify the part size in bytes (e.g., 1 MB)
 
 // Split the file into parts based on size
